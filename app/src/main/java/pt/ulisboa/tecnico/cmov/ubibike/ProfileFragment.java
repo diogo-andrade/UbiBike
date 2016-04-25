cdf3bf709263d433b3292d019d7876674f06aa2d
diogo-andrade <diogo@yubo.be>
2016-04-25 01:05:09 +0100

    Renamed profile name and adjustments in xml layout

--- app/src/main/java/pt/ulisboa/tecnico/cmov/ubibike/ProfileFragment.java
+++ app/src/main/java/pt/ulisboa/tecnico/cmov/ubibike/ProfileFragment.java
@@ -58,7 +58,7 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.profile_template, container, false);
+        return inflater.inflate(R.layout.fragment_profile, container, false);
     }
 /*
     // TODO: Rename method, update argument and hook method into UI event
--- app/src/main/java/pt/ulisboa/tecnico/cmov/ubibike/RegisterActivity.java
+++ app/src/main/java/pt/ulisboa/tecnico/cmov/ubibike/RegisterActivity.java
@@ -2,13 +2,6 @@ package pt.ulisboa.tecnico.cmov.ubibike;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.KeyEvent;
-import android.view.View;
-import android.view.inputmethod.EditorInfo;
-import android.widget.AutoCompleteTextView;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 
 /**
  * Created by Loureiro on 13-04-2016.
@@ -17,7 +10,7 @@ public class RegisterActivity  extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.register_user);
+        setContentView(R.layout.activity_register);
         setTitle("Create Account");
         // Set up the login form.
         /*mEmailView = (AutoCompleteTextView) findViewById(R.id.email);

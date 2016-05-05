c700a34b82718a80d3368e10edc82317c8e4266b
Pedro Loureiro <pedro.loureiro.93@gmail.com>
2016-05-05 15:09:40 +0100

    Register user updates
    back button function included and username removed from form

--- app/src/main/java/pt/ulisboa/tecnico/cmov/ubibike/RegisterActivity.java
+++ app/src/main/java/pt/ulisboa/tecnico/cmov/ubibike/RegisterActivity.java
@@ -2,6 +2,8 @@ package pt.ulisboa.tecnico.cmov.ubibike;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 /**
  * Created by Loureiro on 13-04-2016.
@@ -11,32 +13,15 @@ public class RegisterActivity  extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-        setTitle("Create Account");
-        // Set up the login form.
-        /*mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
-        populateAutoComplete();
 
-        mPasswordView = (EditText) findViewById(R.id.password);
-        mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-            @Override
-            public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
-                if (id == R.id.login || id == EditorInfo.IME_NULL) {
-                    attemptLogin();
-                    return true;
-                }
-                return false;
-            }
-        });
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        Button mEmailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
-        mEmailSignInButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                attemptLogin();
-            }
-        });
+        Bundle extras = getIntent().getExtras();
+    }
 
-        mLoginFormView = findViewById(R.id.login_form);
-        mProgressView = findViewById(R.id.login_progress);*/
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        onBackPressed();
+        return true;
     }
 }

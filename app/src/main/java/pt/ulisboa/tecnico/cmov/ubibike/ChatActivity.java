025d026b39997e52f5901d2080a32d9c5afb806f
Rafael-Barreira <rafael.spam.barreira@gmail.com>
2016-05-05 12:51:43 +0100

    chat update

--- app/src/main/java/pt/ulisboa/tecnico/cmov/ubibike/ChatActivity.java
+++ app/src/main/java/pt/ulisboa/tecnico/cmov/ubibike/ChatActivity.java
@@ -1,14 +1,18 @@
 package pt.ulisboa.tecnico.cmov.ubibike;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.database.DataSetObserver;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.AbsListView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
+
 import pt.ulisboa.tecnico.cmov.ubibike.adapters.ChatArrayAdapter;
 
 
@@ -27,6 +31,15 @@ public class ChatActivity extends Activity {
 
         setContentView(R.layout.chat_activity);
 
+        Log.d("DEBUG","YO");
+        TextView myTextView = (TextView)findViewById(R.id.toolbar_title);
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            Log.d("DEBUG", "YOyo");
+            String name = extras.getString("EXTRA_NAME");
+            myTextView.setText(name);
+        }
+        Log.d("DEBUG", "YOYO");
         buttonSend = (Button) findViewById(R.id.send);
 
         listView = (ListView) findViewById(R.id.msgview);
@@ -62,6 +75,8 @@ public class ChatActivity extends Activity {
             }
         });
     }
+
+
 
     private boolean sendChatMessage() {
         chatArrayAdapter.add(new ChatMessage(side, chatText.getText().toString()));

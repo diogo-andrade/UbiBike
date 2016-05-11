5b748f4b2ea94ad17b2bd01f73620f6d77b52241
Pedro Loureiro <pedro.loureiro.93@gmail.com>
2016-05-11 16:59:06 +0100

    Register with async and progress animation

--- app/src/main/java/pt/ulisboa/tecnico/cmov/ubibike/RegisterActivity.java
+++ app/src/main/java/pt/ulisboa/tecnico/cmov/ubibike/RegisterActivity.java
@@ -1,8 +1,13 @@
 package pt.ulisboa.tecnico.cmov.ubibike;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.app.FragmentTransaction;
 import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -14,6 +19,8 @@ import android.widget.Toast;
 import android.view.View.OnClickListener;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+
+import pt.ulisboa.tecnico.cmov.ubibike.services.UBIClient;
 
 /**
  * Created by Loureiro on 13-04-2016.
@@ -28,6 +35,9 @@ public class RegisterActivity  extends AppCompatActivity {
     public static String P_NAME;
     public static String P_EMAIL;
     public static String P_SCORE;
+    private UserRegisterTask mRegTask = null;
+    private View mProgressView;
+    private View mRegFormView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,19 +55,15 @@ public class RegisterActivity  extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 if(createAccount()) {
-                    Toast.makeText(getApplicationContext(), "Welcome to UbiBikers!!!",
-                            Toast.LENGTH_LONG).show();
-                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                    intent.putExtra("name", P_NAME);
-                    intent.putExtra("email", P_EMAIL);
-                    intent.putExtra("score", P_SCORE);
-                    startActivity(intent);
-                    finish();
+                    showProgress(true);
+                    mRegTask = new UserRegisterTask(name.getText().toString(), email.getText().toString(), pass.getText().toString());
+                    mRegTask.execute((Void) null);
                 }
             }
         });
 
-
+        mRegFormView=findViewById(R.id.registration_form);
+        mProgressView=findViewById(R.id.register_progress);
     }
 
     @Override
@@ -112,5 +118,110 @@ public class RegisterActivity  extends AppCompatActivity {
         P_SCORE="0";
 
         return true;
+    }
+
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
+    private void showProgress(final boolean show) {
+        // On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
+        // for very easy animations. If available, use these APIs to fade-in
+        // the progress spinner.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
+            int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
+
+            mRegFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+            mRegFormView.animate().setDuration(shortAnimTime).alpha(
+                    show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mRegFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+                }
+            });
+
+            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            mProgressView.animate().setDuration(shortAnimTime).alpha(
+                    show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+                }
+            });
+        } else {
+            // The ViewPropertyAnimator APIs are not available, so simply show
+            // and hide the relevant UI components.
+            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            mRegFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+        }
+    }
+
+    public class UserRegisterTask extends AsyncTask<Void, Void, Boolean> {
+
+        private final String mName;
+        private final String mEmail;
+        private final String mPassword;
+
+        UserRegisterTask(String name, String email, String password) {
+            mName = name;
+            mEmail = email;
+            mPassword = password;
+        }
+
+        @Override
+        protected Boolean doInBackground(Void... params) {
+            // TODO: attempt authentication against a network service.
+
+          /*  try {
+                // Simulate network access.
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                return false;
+            }*/
+        //TODO: uncomment this
+          /*  try {
+                UBIClient client = new UBIClient("104.196.28.216",8081);
+                //String result = client.requestLogin(mEmail, mPassword);
+
+                client.close();
+
+                Thread.sleep(2000);
+            } catch (Exception e) {
+                e.printStackTrace();
+                return false;
+            }*/
+
+            /*for (String credential : DUMMY_CREDENTIALS) {
+                String[] pieces = credential.split(":");
+                if (pieces[0].equals(mEmail)) {
+                    // Account exists, return true if the password matches.
+                    return pieces[1].equals(mPassword);
+                }
+            }*/
+
+            // TODO: register the new account here.
+            return true;
+        }
+
+        @Override
+        protected void onPostExecute(final Boolean success) {
+            mRegTask = null;
+
+            if (success) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                intent.putExtra("name", P_NAME);
+                intent.putExtra("email", P_EMAIL);
+                intent.putExtra("score", P_SCORE);
+                startActivity(intent);
+                finish();
+
+            } else {
+                showProgress(false);
+                email.setError("This email is already in use");
+                email.requestFocus();
+            }
+        }
+
+        @Override
+        protected void onCancelled() {
+            mRegTask = null;
+        }
     }
 }
